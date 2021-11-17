0b75583384f9aae5d208a12be0720e240f4e744c
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-11-17 08:59:26 +0700

    basis login page UI

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import { Paper, Box, Typography, Button } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    minHeight: "100vh",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  paper: {
+    padding: 20,
+    width: "50%",
+    margin: "0 auto",
+  },
+}));
 
 const LoginPage = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h3>Login page</h3>
+    <div className={classes.root}>
+      <Paper className={classes.paper} elevation={5}>
+        <Typography variant="h6" align="center">
+          Student management
+        </Typography>
+        <Box width={1} m={1}>
+          <Button variant="contained" fullWidth color="primary">
+            Sign In
+          </Button>
+        </Box>
+      </Paper>
     </div>
   );
 };
